ccf8221d221feccaca429065ba25733e2fcacfeb
billgano <noreply>
2015-03-28 19:05:42 +0000

    Add component Button1

---
+++ ui/button1.reel/button1.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/button1.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Button1
+ * @extends Component
+ */
+exports.Button1 = Component.specialize(/** @lends Button1# */ {
+    constructor: {
+        value: function Button1() {
+            this.super();
+        }
+    }
+});
